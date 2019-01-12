e6f135cd3687c947e8d2963e1d5c8db7a1b76e0e
cwtsang1012 <cwtsang1012@gmail.com>
2019-01-13 03:41:23 +0800

    Create XMLHandling

---
+++ CompareExcelToXMLConsole/CompareExcelToXMLConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareExcelToXMLConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Start.");
+
+            
+            List<string> keyList = new List<string>();
+            keyList.Add("JOBNAME");
+            keyList.Add("MEMNAME");
+            keyList.Add("DESCRIPTION");
+            keyList.Add("RUN_AS");
+            keyList.Add("TASKTYPE");
+            keyList.Add("NODEID");
+            keyList.Add("MEMLIB");
+            keyList.Add("TIMEFROM");
+
+
+            XMLHandling xmlFile = new XMLHandling();
+            var xmlData = xmlFile.ReadXMLData(ConfigurationManager.AppSettings["XMLFilePath"], keyList);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CompareExcelToXMLConsole/CompareExcelToXMLConsole/XMLHandling.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+using System.Xml.Linq;
+
+namespace CompareExcelToXMLConsole
+{
+    public class XMLHandling
+    {
+        public List<Dictionary<string, string>> ReadXMLData(string xmlFilePath, List<string> keyList)
+        {
+            if (!File.Exists(xmlFilePath))
+            {
+                Console.WriteLine("Unable to find the XML File!");
+                return null;
+            }
+            else
+            {
+                List<Dictionary<string, string>> xmlDataList = new List<Dictionary<string, string>>();
+                XDocument doc = XDocument.Load(xmlFilePath);
+                foreach (XElement jobElement in doc.Element("DEFTABLE").Elements("FOLDER").Elements())
+                {
+                    if (jobElement.HasAttributes)
+                    {
+                        Dictionary<string, string> jobDict = new Dictionary<string, string>();
+                        List<XAttribute> attributes = jobElement.Attributes().ToList();
+                        foreach (string key in keyList)
+                        {
+                            var attr = attributes.FindAll(a => a.Name == key);
+                            if (attr.Count > 0)
+                            {
+                                jobDict.Add(key, attr[0].Value.ToString());
+                            }
+
+                            
+                        }
+
+                        if (jobDict.Count > 1)
+                            xmlDataList.Add(jobDict);
+                    }
+                    
+                }
+
+                return xmlDataList;
+            }
+        }
+    }
+}
